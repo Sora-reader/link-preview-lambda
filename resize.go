683622d8b52a19fa7d407ff38927c8e87edd8fcc
dhvcc <1337kwiz@gmail.com>
2021-09-04 18:36:07 +0300

    Removed go-bindata, using Blur instead of logo

--- resize.go
+++ resize.go
@@ -5,7 +5,6 @@ import (
 	"encoding/base64"
 	"fmt"
 	"image"
-	"image/color"
 	"net/http"
 
 	"github.com/disintegration/imaging"
@@ -17,12 +16,6 @@ const minHeight = 250
 
 func ConvertImage(url string) image.Image {
 	fmt.Printf("====> Should convert url: %s.\n", url)
-	asset, err := Asset("assets/logo.png")
-	if err != nil {
-		fmt.Println("Can't load asset, falling back to black square")
-		return imaging.New(width, height, color.Black)
-	}
-	background, _ := imaging.Decode(bytes.NewReader(asset))
 
 	fmt.Printf("Started request.\n")
 	imageResp, err := http.Get(url)
@@ -33,14 +26,21 @@ func ConvertImage(url string) image.Image {
 
 	fmt.Printf("Finished request.\n")
 	image, _ := imaging.Decode(imageResp.Body)
-	image_h := image.Bounds().Max.Y
+	imageH, imageW := image.Bounds().Max.Y, image.Bounds().Max.Y
 
-	if image_h < minHeight {
+	if imageH < minHeight {
 		image = imaging.Resize(image, 0, minHeight, imaging.Lanczos)
-		image_h = image.Bounds().Max.Y
+		imageH = image.Bounds().Max.Y
+	} else if imageH > height || imageW > width {
+		image = imaging.Fit(image, width, height, imaging.Lanczos)
+		imageH = image.Bounds().Max.Y
 	}
 
-	output := imaging.Resize(background, 0, image_h, imaging.Lanczos)
+	bgWidth := int(float64(width) / (float64(height) / float64(imageH)))
+	background := imaging.Resize(image, bgWidth, imageH, imaging.Lanczos)
+	background = imaging.Blur(background, 20)
+
+	output := imaging.Resize(background, 0, imageH, imaging.Lanczos)
 	output = imaging.PasteCenter(output, image)
 	fmt.Printf("====> Returning image %s.\n", output.Bounds().Max)
 	return output
